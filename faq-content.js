f48958207940a5c83faff77540a56bfe9d2dad7f
ericbrotto <77237851+ericbrotto@users.noreply.github.com>
2026-04-14 12:32:07 +0300

    Add files via upload
    updating the faq

---
+++ faq-content.js
@@ -0,0 +1,111 @@
+window.faqContent = `
+<h2>Frequently Asked Questions</h2>
+
+<details>
+  <summary>What is Too Thirsty?</summary>
+  <p>
+    Too Thirsty is a live dating show and party.
+    <br><br>
+    Picture it: one girl is on stage with two guys. She can't see them. They can't see her. She asks them questions to see who she likes best. Then she picks one and they meet. The big questions then are: does she still fancy him? Does she prefer the other one instead? Do both give her the ick? <a href="https://tickets.toothirsty.com/">Buy a ticket</a> to find out!
+    <br><br>
+    After the show finishes, we host an After Party where you can mingle with other singles, and maybe find your own date for the evening!
+  </p>
+</details>
+
+<details>
+  <summary>How do I become a Contestant?</summary>
+  <p>
+    <a href="https://tickets.toothirsty.com/">Buy a ticket</a> and check the box that says you are interested in being a Contestant. After that, send us a <a href="https://wa.me/447544856788">WhatsApp message</a> with a 1 minute voice note saying why you want to be on the show. Also state your age. Lastly, please send us a pic or a link to your social media.
+    <br><br>
+    PLEASE NOTE: Expressing interest in being a Contestant does not guarantee a place on stage. Also, you may retract your application at anytime. Just say you're not feeling it anymore.
+    <br><br>
+    Contestants are chosen the night of.
+  </p>
+</details>
+
+<details>
+  <summary>Is this real? Do people actually get dates on stage?</summary>
+  <p>
+    Yes. 100% of our Contestants have gone on a date.
+  </p>
+</details>
+
+<details>
+  <summary>What is the vibe?</summary>
+  <p>
+    Very wholesome. Also funny.
+  </p>
+</details>
+
+<details>
+  <summary>Are there prizes?</summary>
+  <p>
+    Yes. Contestants can win prizes.
+  </p>
+</details>
+
+<details>
+  <summary>What is the itinerary of the show?</summary>
+  <p>
+    Check your ticket, but usually the show starts at 8, finishes at 9:30 with a party after.
+  </p>
+</details>
+
+<details>
+  <summary>Where is the comedy show and where is the party?</summary>
+  <p>
+    Both the comedy show and the party are at the same location. <a href="https://tickets.toothirsty.com/">Click here</a> for more information on that.
+  </p>
+</details>
+
+<details>
+  <summary>When is your next show?</summary>
+  <p>
+    Click <a href="https://tickets.toothirsty.com/">here to see our upcoming dates</a>. We host a show about once a month.
+  </p>
+</details>
+
+<details>
+  <summary>Do you offer refunds?</summary>
+  <p>
+    You can buy refund protection at the point of sale. This covers refunds under certain circumstances.
+  </p>
+</details>
+
+
+
+<details>
+  <summary>I’m broke. Do you offer discounted tickets for groups?</summary>
+  <p>
+    Almost always. Send us a message on <a href="https://wa.me/447544856788">WhatsApp</a>.
+  </p>
+</details>
+
+<details>
+  <summary>Are the shows filmed? Can I watch them online?</summary>
+  <p>
+    Yes. The shows are filmed and we post them on <a href="https://www.youtube.com/@TooThirsty">YouTube</a>.
+  </p>
+</details>
+
+<details>
+  <summary>I’m from the press. Can I cover you?</summary>
+  <p>
+    Please contact us at team@toothirsty.com or on <a href="https://wa.me/447544856788">WhatsApp</a>.
+  </p>
+</details>
+
+<details>
+  <summary>Do you welcome stag dos or hen dos?</summary>
+  <p>
+    Yes.
+  </p>
+</details>
+
+<details>
+  <summary>How can I contact you?</summary>
+  <p>
+    Email us at team@toothirsty.com or DM us on <a href="https://wa.me/447544856788">WhatsApp</a> at ‪+44 7544 856788‬. Don't message us on social media. We're slow to respond on there.
+  </p>
+</details>
+`;
